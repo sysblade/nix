c7a6388a0456db8dfa7044c1e774e892e7b42fc1
Mathieu Poussin <git@kedare.net>
2025-12-26 12:24:10 +0100

    agenix

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nix-gaming.url = "github:fufexan/nix-gaming";
     home-manager.url = "github:nix-community/home-manager";
+    agenix.url = "github:ryantm/agenix";
   };
 
   outputs =
@@ -13,39 +14,50 @@
       self,
       nixpkgs,
       nixos-hardware,
+      agenix,
       ...
     }@inputs:
     {
       nixosConfigurations = {
         esbcn1-nix-cache1 = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
           system = "x86_64-linux";
           modules = [
             ./hosts/esbcn1-nix-cache1/configuration.nix
+            agenix.nixosModules.default
           ];
         };
         esbcn1-nas1 = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
           system = "x86_64-linux";
           modules = [
             ./hosts/esbcn1-nas1/configuration.nix
+            agenix.nixosModules.default
           ];
         };
         esbcn1-media1 = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
           system = "x86_64-linux";
           modules = [
             ./hosts/esbcn1-media1/configuration.nix
+            agenix.nixosModules.default
           ];
         };
         garuda = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
           system = "x86_64-linux";
           modules = [
             ./hosts/garuda/configuration.nix
             nixos-hardware.nixosModules.framework-13-7040-amd
+            agenix.nixosModules.default
           ];
         };
         carbuncle = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
           system = "x86_64-linux";
           modules = [
             ./hosts/carbuncle/configuration.nix
+            agenix.nixosModules.default
           ];
         };
         bahamut = nixpkgs.lib.nixosSystem {
@@ -53,6 +65,7 @@
           system = "x86_64-linux";
           modules = [
             ./hosts/bahamut/configuration.nix
+            agenix.nixosModules.default
           ];
         };
       };
---
+++ secrets/secrets.nix
@@ -0,0 +1,16 @@
+let
+  # User SSH keys (for managing secrets from workstation)
+  kedare = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICQn06uPFTEWD0XLFPI+z1x73PicyR0wg6i39z4TtlPU";
+
+  # System host keys
+  garuda = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK+TzzoaQ3PPw+zG6e1KwPmGpohET2ojzmUN/c4xTm9h root@nixos";
+
+  # TODO: Add other hosts when their SSH keys are available
+  # esbcn1-nix-cache1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAA... root@esbcn1-nix-cache1";
+  # esbcn1-nas1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAA... root@esbcn1-nas1";
+  # esbcn1-media1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAA... root@esbcn1-media1";
+  # carbuncle = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAA... root@carbuncle";
+  # bahamut = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAA... root@bahamut";
+in
+{
+}
